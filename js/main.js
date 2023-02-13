41a783fd2a71414cf5ff553584d03276102d98d5
isabellaCE <isabellaemiliano14@gmail.com>
2023-02-13 10:31:29 -0300

    feat/Atualizando itens da lista

--- js/main.js
+++ js/main.js
@@ -1,31 +1,53 @@
 const form = document.getElementById("novoItem");
 const lista = document.getElementById("lista");
-const items = [];
+const items = JSON.parse(localStorage.getItem("items")) || [];
+
+items.forEach((element) => {
+  createItem(element.nome, element.quantidade);
+});
 
 form.addEventListener("submit", (evento) => {
   evento.preventDefault();
   const nome = evento.target.elements["nome"];
   const quantidade = evento.target.elements["quantidade"];
 
-  createItem(nome.value, quantidade.value);
+  const existe = items.find((element) => element.nome === nome.value);
+
+  const itemAtual = {
+    nome: nome.value,
+    quantidade: quantidade.value,
+  };
+
+  if (existe) {
+    itemAtual.id = existe.id;
+    atualizarItem(itemAtual);
+    items[existe.id] = itemAtual;
+  } else {
+    itemAtual.id = items.length;
+    createItem(itemAtual);
+    items.push(itemAtual);
+  }
+  localStorage.setItem("items", JSON.stringify(items));
 
   nome.value = "";
   quantidade.value = "";
 });
 
-function createItem(nome, quantidade) {
+function createItem(newItem) {
   const novoItem = document.createElement("li");
   novoItem.classList.add("item");
 
   const quantidadeItem = document.createElement("strong");
-  quantidadeItem.innerHTML = quantidade;
+  quantidadeItem.innerHTML = newItem.quantidade;
+  quantidadeItem.dataset.id = newItem.id;
 
   novoItem.appendChild(quantidadeItem);
-  novoItem.innerHTML += nome;
+  novoItem.innerHTML += newItem.nome;
 
   lista.appendChild(novoItem);
+}
 
-  const item = { nome: nome, quantidade: quantidade };
-  items.push(item);
-  localStorage.setItem("items", JSON.stringify(items));
+function atualizarItem(item) {
+  document.querySelector("[data-id='" + item.id + "']").innerHTML =
+    item.quantidade;
 }
